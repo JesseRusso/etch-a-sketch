57b5e8aa677e61b26b72b01376d02d66d5cf90ba
Jesse Russo <jesserusso90@gmail.com>
2022-08-25 19:44:24 -0700

    grid resize working

--- script.js
+++ script.js
@@ -1,27 +1,41 @@
 let size = 16;
+let isDown = false;
 let gridArray = [];
-document.getElementById("reset-button").addEventListener("click",resetGrid);
+let slider = document.getElementById("range");
+let sliderCount = document.getElementById("slider-value");
 const grid = document.getElementById("grid-container");
+
+
+document.getElementById("reset-button").addEventListener("click",makeGrid);
+slider.addEventListener("change", function(){
+    size = slider.value;
+    sliderCount.innerText = slider.value;
+    makeGrid()
+})
 makeGrid()
+
 function makeGrid(){
+    gridArray.forEach(clearGrid)
+    gridArray = [];
+
     for(let i = 0; i < size * size; i++){
         let div = document.createElement('div');
-        div.className=`"${i}"`;
+        div.className=`${i} uncoloured`;
         div.addEventListener("mouseover", function(){
-            div.setAttribute("style", "background-color:black");
+            div.classList.remove('uncoloured');
+            div.classList.add("coloured");
         });
         gridArray.push(div);
-        document.getElementById('grid-container').appendChild(div);
+        grid.appendChild(div);
     }
+    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 }
-let isDown = false;
+
     document.body.onmousedown = () => (isDown = true);
     document.body.onmouseup = () => (isDown = false);
     
-function resetGrid(){
-    gridArray.forEach(reset);
-}
-function reset(element){
-    element.setAttribute("style", "background-color: white");
-    grid.
+
+function clearGrid(element){
+    grid.removeChild(element);
 }
